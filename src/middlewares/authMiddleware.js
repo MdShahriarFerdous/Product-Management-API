53df024e53df12307f22635fb22c28d4f7b56014
Shahriar Ferdous <shahriarferdous2233@gmail.com>
2023-06-10 13:13:25 +0600

    Aunthenticated or signed in check when inserting product

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -9,12 +9,26 @@ exports.authenticate = async (req, res, next) => {
 		req.headers.auth = decoded;
 		next();
 	} catch (error) {
-		return res.status(401).json(error);
+		return res.status(401).json({status: error.message, failed : "Unauthorized"});
 	}
 };
 
 exports.isAdmin = async (req, res, next) => {
 	try {
+		//when decoding,, two more information will include inside the decode
+		/*we have created a "key", named "auth" for headers and set the decode value
+to the ==> req.headers.auth. it is done in the authenticate function above.
+so the decode or req.headers.auth is getting this object below:
+obj = {
+        "_id": "648410da741570e96e8fb2e1",
+        "iat": 1686376666,
+        "exp": 1686380266
+    } 
+from here we will take only _id to find the user. so the equation is like:
+===>  req.headers.auth = decoded;
+===>  const user = req.headers.auth;
+===>  const userId = user._id; 
+*/
 		const user = req.headers.auth;
 		const userId = user._id;
 		const findUser = await User.findById(userId);
--- src/routes/apiRoute.js
+++ src/routes/apiRoute.js
@@ -6,23 +6,23 @@ const {
 } = require("../controllers/productController");
 
 const { generateToken } = require("../controllers/authController");
-const {viewAllUsers} = require("../controllers/userController");
+const { viewAllUsers } = require("../controllers/userController");
 const { authenticate, isAdmin } = require("../middlewares/authMiddleware");
 
 //simple get products, need no admin check
 // router.get("/products", getProducts);
 
 //admin check get products, this route can only access by admin
-router.get("/products",authenticate, isAdmin, getProducts);
+router.get("/products", authenticate, isAdmin, getProducts);
 
 //token generate
 router.post("/register", generateToken);
 
-//Insert product details
-router.post("/products",  insertProductDetails);
+//Insert product details when user is signed in
+router.post("/products", authenticate, insertProductDetails);
 
 //Authenticate route
-router.get("/authentication", authenticate, (req, res) => { 
+router.get("/authentication", authenticate, (req, res) => {
 	const user = req.headers.auth;
 	res.json({ ok: true, message: "Authenticated!", user });
 });
